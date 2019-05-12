606e9cee8e8238dc5dff9e1545fd07aabd79f422
DuckNuckTv <50536854+DuckNuckTv@users.noreply.github.com>
2019-05-11 21:39:40 -0700

    Added Person.Java

---
+++ src/Person.java
@@ -0,0 +1,16 @@
+
+public class Person {
+	public int age;
+	public String FirstName;
+	public String MiddleName;
+	public String LastName;
+	public boolean IsMarried;
+	public boolean IsDead;
+	public boolean IsEmployed;
+	public int NumberOfChildren;
+	public int NumberOfPets;
+    
+
+	
+
+                    }
