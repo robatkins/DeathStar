69a0475b4c73e763e1937456266dc374cbb778fc
DuckNuckTv <50536854+DuckNuckTv@users.noreply.github.com>
2019-05-11 22:37:59 -0700

    Added SimCard class

---
+++ src/SimCard.java
@@ -0,0 +1,13 @@
+
+public class SimCard 
+{
+
+	private int iccid;
+	
+	private String imsi;
+	private String secretkey;
+	private Strink puk;
+	private String challenge;
+	private String response;
+
+}
